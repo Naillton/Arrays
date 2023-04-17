232ee4f2d5a6681636007ef2fa6a8eb28be6295d
Nailton Junior <51039019+Naillton@users.noreply.github.com>
2023-04-17 17:12:12 -0300

    finish

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val array1: Array<Int> = arrayOf(1, 2, 3, 4)
+    val array2: Array<Int> = arrayOf(1, 2, 3, 4)
+    var arrayResult: String = ""
+    // usando o withIndex() para recuperar o indice do array e como ambos tem o mesmo tamanho
+    // so precisa fazer a operacao de multiplicacao interando sobre cada indice
+    for (indice in array1.withIndex()) {
+        arrayResult += (array1[indice.index] * array2[indice.index])
+        arrayResult += " "
+    }
+    print(arrayResult)
 }
